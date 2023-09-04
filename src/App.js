6cd46551bf8f281d219bcad0c4043cd8866a6cf9
Reyaz <riyaz.shaik.1481@gmail.com>
2023-09-04 06:37:00 +0530

    loginFormForChat

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.scss';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
-import SignUp from './components/Login/SignUp';
+import SignUp from './components/Login/SignUp'
 
 function App() {
   return (
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './login.scss';
 
 function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
   return (
     <div className='Login_head'>
        <h4>Welcome to Login</h4>
+       <form className='form' onSubmit={handleSubmit}>
+        <label htmlFor='email'>Enter Email</label>
+        <input id='email' name='email' type='text' placeholder="Ex:JohnDalton@gmail.com"/>
+        <label htmlFor='password'>Enter Password</label>
+        <input id='password' name='password' type='password' placeholder='******'/>
+        <label>Give Profile Pic</label>
+        <input type='file' accept='images/*'/>
+        <button>Submit</button>
+       </form>
     </div>
   )
 }
